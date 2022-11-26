cab1dd9277c578ed3cd453b152d546d456e304e1
Gabriel <gabriel.scardozo7@gmail.com>
2022-11-26 17:14:02 -0300

    fix:correcao bug localstorage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,11 +5,6 @@ import Sections from "./components/Sections";
 
 
 export default function Home() {
-
-    useEffect((
-        localStorage.setItem("itensList",JSON.stringify([]))
-    ),[])
-
     return(
         <>
 
